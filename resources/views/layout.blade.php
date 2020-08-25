adae77afa2e096392fbd17bc53e66e475509e919
lucianobuson <lucianobuson@bol.com.br>
2020-08-25 16:03:53 -0300

    ajustando pag. layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,31 +6,55 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pedidos PHP</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 <body>
 {{--    <div class="container">--}}
-        <div class="jumbotron p-3">
+        <div class="jumbotron p-3 mb-1">
             <h1>@yield('cabecalho')</h1>
             <h1></h1>
         </div>
 {{--    </div>--}}
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2 d-flex justify-content-between">
-        <a class="navbar navbar-expand-lg" href="#">Home</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Cadastros
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="{{ route('home') }}">Clientes</a>
+                        <a class="dropdown-item" href="#">Produtos</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pedidos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ajuda</a>
+                </li>
+            </ul>
             @auth
                 <a href="#" class="text-danger">Sair</a>
             @endauth
             @guest
                 <a href="#">Entrar</a>
             @endguest
-{{--        <a class="navbar navbar-expand-lg" href="{{ route('pedidosphp') }}">Home</a>--}}
-{{--        @auth--}}
-{{--            <a href="/sair" class="text-danger">Sair</a>--}}
-{{--        @endauth--}}
-{{--        @guest--}}
-{{--            <a href="/entrar">Entrar</a>--}}
-{{--        @endguest--}}
+            {{--        @auth--}}
+            {{--            <a href="/sair" class="text-danger">Sair</a>--}}
+            {{--        @endauth--}}
+            {{--        @guest--}}
+            {{--            <a href="/entrar">Entrar</a>--}}
+            {{--        @endguest--}}
+        </div>
     </nav>
     @yield('conteudo')
 </body>
